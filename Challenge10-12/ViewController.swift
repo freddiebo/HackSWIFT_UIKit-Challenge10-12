86db65e0148a21d42180d99174bcc71fa63e4d75
VBondarev <freddiebo@yandex.ru>
2020-02-18 12:40:44 +0300

    Initial Commit

---
+++ Challenge10-12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge10-12
+//
+//  Created by  Vladislav Bondarev on 18.02.2020.
+//  Copyright © 2020 Vladislav Bondarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
